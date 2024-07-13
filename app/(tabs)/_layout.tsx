968453e187dd8ccb455361fcf69a8e310b4fbcd7
Nadim Sheikh <nadim@cigitechnologies.com>
2024-07-13 16:22:47 +0530

    add about screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
